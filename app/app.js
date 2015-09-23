6e4a3176af97af3d052649ea68d873d7ac056a4a
Christian Kohler <kohler.christian@gmail.com>
2015-09-23 21:29:47 +0200

    movies done

--- app/app.js
+++ app/app.js
@@ -1,5 +1,64 @@
 
-class MovieTile extends HTMLElement {
+class MoviesView extends HTMLElement {
+    createdCallback() {
+        var dummydata = {
+            movies: [
+                {
+                    imdbID: "tt4733046",
+                    imdbUrl: "http://www.imdb.com/title/tt4733046",
+                    imdbVotes: "10.0",
+                    omdbImgUrl: "http://img.omdbapi.com/?i=tt4733046",
+                    runtimeOriginal: "124 min",
+                    title: "The Weight of Chains 2"
+                },
+                {
+                    imdbID: "tt4008866",
+                    imdbUrl: "http://www.imdb.com/title/tt4008866",
+                    imdbVotes: "6.7",
+                    omdbImgUrl: "http://img.omdbapi.com/?i=tt4008866",
+                    runtimeOriginal: "137 min",
+                    title: "Bibi & Tina: Voll verhext!"
+                }
+            ]
+        };
+
+
+        this.render(dummydata);
+    }
+
+    render(data) {
+        this.innerHTML = `
+
+       <div class="container">
+            <ul class="movies">
+                ${data.movies.map(this.renderMovie).join("")}
+            </ul>
+       </div>
+        `;
+    }
+
+    renderMovie(movie) {
+        return `
+          <li is="movie-tile"
+            imdb-url="${movie.imdbUrl}"
+            image-url="${movie.omdbImgUrl}"
+            title="${movie.title}"
+            runtime="${movie.runtimeOriginal}"
+            rating="${movie.imdbVotes}">
+          </li>
+        `;
+    }
+}
+
+document.registerElement('movies-view', {
+    prototype: MoviesView.prototype
+});
+
+
+
+
+
+class MovieTile extends HTMLLIElement {
 
     createdCallback() {
         this.classList.add("movie__item");
@@ -25,5 +84,6 @@ class MovieTile extends HTMLElement {
 }
 
 document.registerElement('movie-tile', {
-    prototype: MovieTile.prototype
+    prototype: MovieTile.prototype,
+    extends: 'li'
 });
